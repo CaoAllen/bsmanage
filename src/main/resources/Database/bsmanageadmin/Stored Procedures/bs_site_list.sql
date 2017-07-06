363e704fd904f20c95e05226386ab94f682aea50
Allen.cao <289941516@qq.com>
2017-07-07 00:13:48 +0800

    fix sp issue

--- src/main/resources/Database/bsmanageadmin/Stored Procedures/bs_site_list.sql
+++ src/main/resources/Database/bsmanageadmin/Stored Procedures/bs_site_list.sql
@@ -1,6 +1,6 @@
 DROP PROCEDURE IF EXISTS `bs_site_list`;
 
-CREATE DEFINER = `root`@`localhost` PROCEDURE `bs_site_list`(IN _name VARCHAR (100),
+CREATE PROCEDURE `bs_site_list`(IN _name VARCHAR (100),
 	IN _price_low DECIMAL (19, 2),
 	IN _price_high DECIMAL (19, 2),
 	IN _site_type VARCHAR (100))
@@ -9,19 +9,19 @@ BEGIN
 		s.site_id AS siteId,
 		s.NAME AS name,
 		s.flowrate AS flowrate,
+		p.amount as amount,
 		s.site_type AS siteType,
 		s.STATUS AS status,
 		a.address_detail AS addressDetail,
-CASE when s
+		IFNULL(ss.sales_volumn,0) as salesVolumn
 
 	FROM
-		site s,
-		address a
-	WHERE
-		s.site_id = a.site_id
-	AND (
+		site s 
+	LEFT JOIN (price p, address a, sales ss) on (s.site_id = p.site_id and s.site_id = a.site_id and s.site_id = ss.site_id)
+
+	WHERE (
 		s.NAME LIKE CONCAT('%' ,_name, '%')
-		OR _name IS NULL
-	);
+		OR _name IS NULL)
+	GROUP BY s.site_id;
 END;
 
